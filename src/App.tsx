3d2ee5aaf976096e49f49116fa02444cb73c2f1a
Sadykov Arsen <LoCo10291998@gmail.com>
2025-01-07 17:34:42 +0600

    last commit. all is done.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {IBlock} from "./types";
 
 const App = () => {
   const [blocks, setBlocks] = useState<IBlock[]>([]);
+  const [tries, setTries] = useState<number>(0);
+  const [victory , setVictory] = useState(false);
 
   const createBoard = () => {
     const blocksArray: IBlock[]  = [];
@@ -15,16 +17,38 @@ const App = () => {
         class: 'block',
       });
     }
-    let randomIndex = Math.floor(Math.random() * blocksArray.length);
+    const randomIndex = Math.floor(Math.random() * blocksArray.length);
     blocksArray[randomIndex].hasItem = true;
     setBlocks(blocksArray);
   };
+
 
   if(blocks.length === 0){
     createBoard();
   } else {
     console.log(blocks);
   }
+  const clickOnBlock = (index: number) =>{
+    setBlocks(blocks.map(block => {
+      if(block.id === index) {
+        if(victory === false) {
+          if(block.hasItem === false) {
+            if(block.class === 'block') {
+              setTries(prevState => prevState + 1);
+              return {...block, class: 'clicked'};
+            }
+          } else if(block.hasItem === true) {
+            setVictory(true);
+            return {...block, class: 'win'};
+          }
+        } else {
+          alert('You Win!!! Congratulations! \n refresh this site')
+        }
+      }
+
+      return block;
+    }))
+  };
 
   return (
       <>
@@ -33,6 +57,7 @@ const App = () => {
               <div onClick={() => clickOnBlock(block.id)} key={block.id} className={block.class}></div>
           ))};
         </div>
+        <h2><strong>Tries:</strong>{tries}</h2>
       </>
   )
 };
